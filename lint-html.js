cd7cf6d8df13b13502c8b045e607c102323e7857
williamlacy <williamlacy@ucds.email>
2017-11-24 11:42:47 +0000

    remove grunt from interface

--- lint-html.js
+++ lint-html.js
@@ -1,4 +1,3 @@
-const gulp = require('gulp');
 const gutil = require('gulp-util');
 const htmlLinkCrawler = require('html-link-crawler');
 const LintTargetBlank = require('lint-target-blank');
@@ -9,33 +8,31 @@ const LintTargetBlank = require('lint-target-blank');
  * @param taskName
  * @param ignoreQsParams
  */
-module.exports = (taskName, ignoreQsParams=[]) => {
-  gulp.task('lint-all-html', () => new Promise((accept, reject) => {
-    process.env = process.env || 'TEST';
-    require('../bin/www')
-      .on('listening', addr => {
-        const host = addr.address === '::' ? 'localhost' : addr.address;
-        const lintTargetBlank = new LintTargetBlank({});
-        htmlLinkCrawler({ url: `http://${host}:${addr.port}/`, ignoreQsParams })
-          .on('htmlFetchComplete', htmlMap => {
-            const errorStr = Object.keys(htmlMap)
-              .map(url => ({ url, errors: lintTargetBlank.lint(htmlMap[url]) }))
-              .filter(({ errors }) => errors.length > 0)
-              .map(({ url, errors }) => `\t${url}\n\t\t${errors.map(e => e.element)
-                .join('\n\t\t')}`)
-              .join('\n');
-            if (errorStr) {
-              gutil.log(gutil.colors.red(
-                `The following elements require rel="noreferrer nofollow"\n\n${errorStr}`
-              ));
-              process.exit(1);
-              reject();
-            } else {
-              process.exit(0);
-              accept();
-            }
-          })
-          .start();
-      });
-  }));
-};
+module.exports = ({server, ignoreQsParams=[]}) => new Promise((accept, reject) => {
+  process.env = process.env || 'TEST';
+  server
+    .on('listening', addr => {
+      const host = addr.address === '::' ? 'localhost' : addr.address;
+      const lintTargetBlank = new LintTargetBlank({});
+      htmlLinkCrawler({ url: `http://${host}:${addr.port}/`, ignoreQsParams })
+        .on('htmlFetchComplete', htmlMap => {
+          const errorStr = Object.keys(htmlMap)
+            .map(url => ({ url, errors: lintTargetBlank.lint(htmlMap[url]) }))
+            .filter(({ errors }) => errors.length > 0)
+            .map(({ url, errors }) => `  ${url}\n    ${errors.map(e => e.element)
+              .join('\n    ')}`)
+            .join('\n');
+          if (errorStr) {
+            gutil.log(gutil.colors.red(
+              `The following elements require rel="noreferrer nofollow"\n\n${errorStr}`
+            ));
+            process.exit(1);
+            reject();
+          } else {
+            process.exit(0);
+            accept();
+          }
+        })
+        .start();
+    });
+});
