04b5ea4bdc6ddbd0abb3f201c1a5c9592c6382d7
williamlacy <williamlacy@ucds.email>
2017-11-27 10:27:42 +0000

    un-gruntify tasks for generic use

--- lint-html.js
+++ lint-html.js
@@ -1,4 +1,3 @@
-const gutil = require('gulp-util');
 const htmlLinkCrawler = require('html-link-crawler');
 const LintTargetBlank = require('lint-target-blank');
 
@@ -8,31 +7,21 @@ const LintTargetBlank = require('lint-target-blank');
  * @param taskName
  * @param ignoreQsParams
  */
-module.exports = ({server, ignoreQsParams=[]}) => new Promise((accept, reject) => {
-  process.env = process.env || 'TEST';
-  server
-    .on('listening', addr => {
-      const host = addr.address === '::' ? 'localhost' : addr.address;
-      const lintTargetBlank = new LintTargetBlank({});
-      htmlLinkCrawler({ url: `http://${host}:${addr.port}/`, ignoreQsParams })
-        .on('htmlFetchComplete', htmlMap => {
-          const errorStr = Object.keys(htmlMap)
-            .map(url => ({ url, errors: lintTargetBlank.lint(htmlMap[url]) }))
-            .filter(({ errors }) => errors.length > 0)
-            .map(({ url, errors }) => `  ${url}\n    ${errors.map(e => e.element)
-              .join('\n    ')}`)
-            .join('\n');
-          if (errorStr) {
-            gutil.log(gutil.colors.red(
-              `The following elements require rel="noreferrer nofollow"\n\n${errorStr}`
-            ));
-            process.exit(1);
-            reject();
-          } else {
-            process.exit(0);
-            accept();
-          }
-        })
-        .start();
-    });
+module.exports = ({url, ignoreQsParams=[]}) => new Promise((accept, reject) => {
+  const lintTargetBlank = new LintTargetBlank({});
+  htmlLinkCrawler({ url, ignoreQsParams })
+    .on('htmlFetchComplete', htmlMap => {
+      const errorStr = Object.keys(htmlMap)
+        .map(url => ({ url, errors: lintTargetBlank.lint(htmlMap[url]) }))
+        .filter(({ errors }) => errors.length > 0)
+        .map(({ url, errors }) => `  ${url}\n    ${errors.map(e => e.element)
+          .join('\n    ')}`)
+        .join('\n');
+      if (errorStr) {
+        reject(`The following elements require rel="noreferrer nofollow"\n\n${errorStr}`);
+      } else {
+        accept();
+      }
+    })
+    .start();
 });
